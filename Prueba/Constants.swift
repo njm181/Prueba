8949c69bc0b1c35e7353079394ad72cd5910d906
Nicolas Molina <nicolasjmolina1@gmail.com>
2024-08-07 20:46:56 -0300

    Initial Commit

---
+++ Prueba/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Prueba
+//
+//  Created by Nico Molina on 07/08/2024.
+//
+
+import Foundation
+
+struct Constants {
+    public let nombre = "Hi from Constants"
+}
---
+++ Prueba/Prueba.h
@@ -0,0 +1,18 @@
+//
+//  Prueba.h
+//  Prueba
+//
+//  Created by Nico Molina on 07/08/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Prueba.
+FOUNDATION_EXPORT double PruebaVersionNumber;
+
+//! Project version string for Prueba.
+FOUNDATION_EXPORT const unsigned char PruebaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Prueba/PublicHeader.h>
+
+
